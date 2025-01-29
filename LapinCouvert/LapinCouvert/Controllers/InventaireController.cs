d06b68370d19f4b3c32d8af2749a9fea6ab24bf0
Victor <2250292@cegepmontpetit.ca>
2025-01-29 10:51:44 -0500

    test

---
+++ LapinCouvert/LapinCouvert/Controllers/InventaireController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LapinCouvertMVC.Controllers
+{
+    public class InventaireController : Controller
+    {
+        // GET: InventaireController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: InventaireController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InventaireController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InventaireController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventaireController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InventaireController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InventaireController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InventaireController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LapinCouvert/Models/Models/Produit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Produit
+    {
+        public int ProduitId { get; set; }
+        public string Nom { get; set; }
+        public decimal Prix { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Quantite { get; set; }
+
+        public decimal PrixCoutant { get; set; }
+        public bool Disponible { get; set; }
+        public CategorieEnum Categorie { get; set; }
+    }
+}
